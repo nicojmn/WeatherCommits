9cdb3e7f7e8b54bd70530faab790803c67994a67
nicojmn <nicolasjeanmenne@gmail.com>
2025-08-13 17:02:46 +0200

    Retrieve user from url params

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,17 +4,19 @@ import type { PageServerLoad } from './$types';
 
 let dates: string[] = []
 
-const repos = await listPublicRepos("nicojmn");
-repos.forEach((repo) => {
-    getCommits(repo).then((commits: Commit[]) => {
-        commits.forEach((commit => {
-            commitToDate(commit).then(date => {
-                dates.push(date)
-            })
-        }))
-    })
-})
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ url }) => {
+    let user = url.searchParams.get('user') || "octocat"; // fallback to octocat if no user is provided to avoid errors
+
+    const repos = await listPublicRepos(user);
+    repos.forEach((repo) => {
+        getCommits(repo).then((commits: Commit[]) => {
+            commits.forEach((commit => {
+                commitToDate(commit).then(date => {
+                    dates.push(date)
+                })
+            }))
+        })
+    })
     return { dates };
 }
