5df301d45e40ff792632784f0ecadef223f7ac8d
nicojmn <nicolasjeanmenne@gmail.com>
2025-08-13 22:53:33 +0200

    Reworked load

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,19 +2,18 @@ import { listPublicRepos, getCommits, commitToDate } from "$lib/github"
 import type { Commit } from "$lib/types/github"
 import type { PageServerLoad } from './$types';
 
-let dates: string[] = []
-
-
 export const load: PageServerLoad = async ({ url }) => {
+    let dates: string[] = []
     let user = url.searchParams.get('user') || "octocat"; // fallback to octocat if no user is provided to avoid errors
 
     const repos = await listPublicRepos(user);
-    repos.forEach((repo) => {
-        getCommits(repo, user).then((commits: Commit[]) => {
-            commits.forEach((commit => {
-                dates.push(commit.date)
-            }))
-        })
+    const commits = await Promise.all(
+        repos.map((repo) => getCommits(repo, user))
+    )
+    commits.flat().forEach((commit: Commit) => {
+        commitToDate(commit).then((date => {
+            dates.push(date)
+        }))
     })
     return { dates };
 }
